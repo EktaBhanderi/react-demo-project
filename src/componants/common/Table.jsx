86e4e568e9c2f7762574e30d492948962b862055
EktaBhanderi <ektaakabari985@gmail.com>
2022-12-02 09:52:18 +0530

    init changes

--- src/componants/common/Table.jsx
+++ src/componants/common/Table.jsx
@@ -8,6 +8,7 @@ const Table = (props) => {
     <table className="table">
       <TableHeader columns={columns} sortColumn={sortColumn} onSort={onSort} />
       <TableBody data={data} columns={columns} />
+      <div>Hello</div>
     </table>
   );
 };
